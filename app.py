70521345bcd1f2e921e5cc995e398fbb155eaa5b
TomWerner <tom-werner@uiowa.edu>
2015-08-08 19:32:26 -0500

    Added a test post endpoint

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/execute', methods=['POST'], )
+def execute():
+    if request.method != 'POST':
+        return
+    return request.get_json()
+
 
 if __name__ == "__main__":
     app.run()
