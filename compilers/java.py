2598a70e253e53b067bb85cc0e073510f28654b3
TomWerner <tom-werner@uiowa.edu>
2015-08-08 23:06:42 -0500

    Java now cleans up after itself

--- compilers/java.py
+++ compilers/java.py
@@ -9,16 +9,15 @@ def execute(filename, code, inputs):
     :param inputs: The list of STDIN inputs to test this program on
     :return: JSON object with the result, error status, message, and outputs
     """
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-    os.chdir("../code")
+    setup()
     with open(filename + '.java', mode='w+') as writer:
         writer.write(code)
 
     p = Popen(['javac', filename + '.java'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate()
-    os.remove(filename + '.java')
 
     if len(err) > 0:
+        cleanup(filename)
         return json.dumps({'result': 'Compilation Error', 'error': True, 'message': err, 'output': []})
 
     outputs = []
@@ -28,8 +27,32 @@ def execute(filename, code, inputs):
         output, err = p.communicate(input_)
         outputs.append(output.strip())
         if len(err) > 0:
-            os.remove(filename + '.class')
+            cleanup(filename)
             return json.dumps({'result': 'Runtime Error', 'error': True, 'message': err, 'output': outputs})
 
-    os.remove(filename + '.class')
+    cleanup(filename)
     return json.dumps({'result': 'Ran Successfully', 'error': False, 'message': '', 'output': outputs})
+
+
+def setup():
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    os.chdir("..")
+
+    try:
+        os.mkdir("code")
+    except OSError:
+        pass
+    os.chdir("code")
+
+
+def cleanup(filename):
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    os.chdir("../code")
+    for the_file in os.listdir(os.curdir):
+        try:
+            os.remove(the_file)
+        except OSError:
+            pass
+
+    os.chdir('..')
+    os.rmdir('code')
